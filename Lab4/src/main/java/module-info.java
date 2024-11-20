8694b13f2433202060d295dba87607fded5618eb
nihar4696 <ndholar1@my.centennialcollege.ca>
2024-11-19 19:03:26 -0500

    Lab4

--- Lab4/src/main/java/module-info.java
+++ Lab4/src/main/java/module-info.java
@@ -1,19 +1,10 @@
 module com.example.lab4 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
 
     opens com.example.lab4 to javafx.fxml;
     exports com.example.lab4;
 
-    // This exports the package so JavaFX can access it
 
 }
